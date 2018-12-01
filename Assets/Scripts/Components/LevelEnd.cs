1ebddc5a59839213da64180a575c408f7f4fe74c
hinomi <nicolas.treiber@gmail.com>
2018-12-01 19:33:48 +0100

    Level end component & prefab

---
+++ Assets/Scripts/Components/LevelEnd.cs
@@ -0,0 +1,26 @@
+﻿using Gamekit2D;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Components {
+	public class LevelEnd : MonoBehaviour {
+		#region Private fields
+		[SerializeField, SceneName] private string nextLevel;
+		[SerializeField] private Canvas canvas;
+		#endregion
+
+		#region Life cycle
+		private void OnTriggerEnter2D(Collider2D coll) {
+			Time.timeScale = 0;
+			canvas.gameObject.SetActive(true);
+		}
+		#endregion
+
+		#region Public methods
+		public void LoadNextLevel() {
+			Time.timeScale = 1;
+			SceneManager.LoadScene(nextLevel);
+		}
+		#endregion
+	}
+}
