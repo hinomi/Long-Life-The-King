ba80d6f18131abe7083ced3682b5645e302d14f1
hinomi <nicolas.treiber@gmail.com>
2018-12-01 13:03:39 +0100

    Added auto move component

---
+++ Assets/Scripts/Components/AutoMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Components {
+	[RequireComponent(typeof(GroundChecker))]
+	public class AutoMove : MonoBehaviour {
+		#region Private fields
+		[SerializeField] private float speed = 2f;
+
+		private Rigidbody2D body;
+		private GroundChecker groundChecker;
+		#endregion
+
+		#region Life cycle
+		private void Awake() {
+			body = GetComponent<Rigidbody2D>();
+			groundChecker = GetComponent<GroundChecker>();
+		}
+	
+		private void FixedUpdate() {
+			if (groundChecker.Grounded)
+				body.velocity = Vector2.right * speed;
+		}
+		#endregion
+	}
+}
